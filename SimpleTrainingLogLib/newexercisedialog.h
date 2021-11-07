91301da9a0ad14817da1b92552d72711f5483b0a
Ferenc Kovacs <derefer@gmail.com>
2021-11-07 16:32:03 +0100

    Remove some useless comments, replace include guards with #pragma once.

--- SimpleTrainingLogLib/newexercisedialog.h
+++ SimpleTrainingLogLib/newexercisedialog.h
@@ -1,25 +1,4 @@
-/***************************************************************************
- *   Copyright (C) 2008, 2009 by Ferenc Kovács                             *
- *   derefer@gmail.com                                                     *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
- ***************************************************************************/
-
-#ifndef NEWEXERCISEDIALOG_H_
-#define NEWEXERCISEDIALOG_H_
+#pragma once
 
 #include <QComboBox>
 #include <QDialog>
@@ -112,5 +91,3 @@ private:
     QListWidget *placesListWidget;
     QList<QCheckBox*> m_weathersCheckBoxList;
 };
-
-#endif  // NEWEXERCISEDIALOG_H_
--- SimpleTrainingLogLib/sportsdialog.h
+++ SimpleTrainingLogLib/sportsdialog.h
@@ -1,25 +1,4 @@
-/***************************************************************************
- *   Copyright (C) 2008, 2009, 2010 by Ferenc Kovács                                   *
- *   derefer@gmail.com                                                     *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
- ***************************************************************************/
-
-#ifndef SPORTSDIALOG_H_
-#define SPORTSDIALOG_H_
+#pragma once
 
 #include <QDialog>
 
@@ -76,5 +55,3 @@ private:
     QPushButton *m_cancelPushButton;
     QPushButton *m_colorPushButton;
 };
-
-#endif  // SPORTSDIALOG_H_
